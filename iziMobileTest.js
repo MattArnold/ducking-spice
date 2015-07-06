7b3d35fe695d5a22430bbdd99e956e3b9f6836ee
Matt Arnold <matt.mattarn@gmail.com>
2015-07-06 13:17:33 -0400

    POST handlers for login and logout without database.

--- iziMobileTest.js
+++ iziMobileTest.js
@@ -1,8 +1,25 @@
-var http = require('http');
-var url = require('url') ;
-var request = require('request');
-var express = require('express');
+var http       = require('http');
+var url        = require('url') ;
+var request    = require('request');
+var express    = require('express');
+var bodyParser = require("body-parser");
+
+var loginform = '<form id="login" action="login" method="post">Username: <input type="text" name="username" id="username" /> <input type="submit" value="Submit" /></form>';
+var logoutform = '<form id="logout" action="logout" method="post"><input type="submit" value="Logout"></form>';
+var loggedin = false;
+
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/login', function(req, res){
+	loggedin = true;
+	res.send(logoutform + '<br><br>Logged in as ' + req.body.username);
+});
+
+app.post('/logout', function(req, res, next){
+	loggedin = false;
+	res.redirect('/');
+});
 
 app.get('/', function(req, res) {
 	var urlQuery = url.parse(req.url, true).query.q;
@@ -10,13 +27,13 @@ app.get('/', function(req, res) {
 	var gif;
 
 	request(endPoint, function(err, resp, body) {
+		var form = loggedin ? logoutform : loginform;
 		body = JSON.parse(body);
-		gif = '<img src="' + body.data.images.fixed_height.url + '">';
-		res.send('Username: <input type="text"><br><br>' + gif);
+		var gif = loggedin ? '<img src="' + body.data.images.fixed_height.url + '">' : '<div>Unauthorized. Please log in.</div>';
+		res.send(form + '<br><br>' + gif);
 	});
 
 });
 
 app.listen(8080);
-
 console.log('Listening on port 8080.');
